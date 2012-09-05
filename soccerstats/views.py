ae2476b03023632c8d9a25d9aca3df4d714cfd32
sh4r3m4n <shareman1204@gmail.com>
2012-09-04 21:11:17 -0300

    Cambios en el diseño

--- soccerstats/views.py
+++ soccerstats/views.py
@@ -6,7 +6,23 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-@login_required
+from django.http import HttpResponse
+from soccerstats.models import *
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+@login_required(login_url = '/admin/login/')
+def admin(request):
+	return render_to_response('soccerstats/main.html',
+		{'partidos' : Partido.objects.all()},
+		RequestContext(request))
+
+@login_required(login_url = '/admin/login/')
+def partido_adm(request, match_id):
+	return HttpResponse('En construccion')
 def admin(request):
 	return render_to_response('soccerstats/main.html',
 		{'partidos' : Partido.objects.all()},

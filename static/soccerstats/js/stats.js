9ca6cf336f94c64e05b8dc7b3ea6665b87b52a06
Matias Lang <shareman1204@gmail.com>
2012-09-13 13:34:17 -0300

    Dejamos de usar sistema provisorio de estadísticas

--- static/soccerstats/js/stats.js
+++ static/soccerstats/js/stats.js
@@ -1,12 +1,30 @@
 posesion = {}
+latiene = false;
 
 $('.player').click(function(){
 	if ($(this).hasClass('player')){
-		if( $(this).hasClass('seleccionado') ){
+		if( ! $(this).hasClass('seleccionado') ){
+			tmppos = new Date().getTime() - tmptime;
+			if (posesion[latiene.pk]){
+				posesion[latiene.pk] += tmppos;
+			}else{
+				posesion[latiene.pk] = tmppos;
+			}
 			latiene = false;
+			tmptime = new Date().getTime();
 		}else{
+			if (latiene !== false){
+				tmppos = new Date().getTime() - tmptime;
+				if (posesion[latiene.pk]){
+					posesion[latiene.pk] += tmppos;
+				}else{
+					posesion[latiene.pk] = tmppos;
+				}
+			}
+			tmptime = new Date().getTime();
 			latiene = jugadores[parseInt($(this).attr('id'))]
 		}
 	}
 	return false;
 });
+
